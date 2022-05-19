9bd2015c4d7788d4c2e38c2815647007e4812d37
idris <64688223+idrisT11@users.noreply.github.com>
2022-05-20 00:45:21 +0200

    test

--- isep-web/model/adminModel.php
+++ isep-web/model/adminModel.php
@@ -63,20 +63,4 @@
 
     
 
-    function db_login(){
-        $servername = 'localhost';
-        $sql_username = 'root';
-        $sql_password = '';
-
-        $db_name = 'iclald';
-
-        $connexion = new mysqli($servername, $sql_username, $sql_password, $db_name);
-
-
-        if($connexion->connect_error){
-            die('Erreur : ' .$conn->connect_error);
-        }
-
-        return $connexion;
-    }
 ?>
