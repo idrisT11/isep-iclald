c9e8dce944a1025c107a17aff08e82424390cf56
Leamartin13 <95086396+Leamartin13@users.noreply.github.com>
2022-05-31 13:12:10 +0200

    m

---
+++ entree.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./static/style/acceuil.css">
+    <link rel="stylesheet" href="./static/fonts/font.css">
+    
+    <link rel="stylesheet" href="./static/style/a.css">
+
+    <title>Acceuil</title>
+</head>
+<body>
+
+
+    <div id="window">
+        <div id="navi">
+        <nav id="nav">
+            <img src="./static/image/IM.png" width="50px" style="position: absolute">
+            <div id="nav_ctn">
+                <a href="./" class="nav_link"> <img src="./static/image/home.png" alt="home"></a>
+                <a href="./dash" class="nav_link"><img src="./static/image/graph.png" alt="dashboard"></a>
+                <a href="./forum" class="nav_link"><img src="./static/image/forum.png" alt="forum"></a>
+                <a href="./map" class="nav_link"> <img src="./static/image/map.png" alt="map"></a>
+            </div>
+        </nav>
+        </div>
+
+        <div id="imp" class="droite">
+            <div id="haut1" >
+                <div id="headr_title" ><h1 >Bienvenue</h1>  
+                </div>
+                <div id="info1"> <p> Votre bien-être, notre métier </p>
+                </div>
+            </div>
+            
+            <div id="container" >
+                <div class="inner-content">
+                    <section class="container reveal">
+                    <div class="text-container">
+                        <div id="txt" class="text-box gros" >
+                            <h3>Le système</h3>
+                            <p>Notre système constituté de différents capteurs, 
+                                vous pourrez consulter les différentes informations enregistrés de votre environnement. 
+                                La température, le CO2 et l'humidité sont des données accessible sur le site internet.</p>
+                        </div>
+                        <div id="b2" class="text-box"><img src="./static/image/the-ben-capteur-co2.jpeg"> 
+                        </div>
+                    </div>
+                    </section>
+                </div>
+                
+                <div class="inner-content">
+                    <section class="container reveal">
+                    <div class="text-container">
+                        <div class="text-box gros2"><img src="./static/image/halo.jpeg"> 
+                        </div>
+                        <div class="text-box">
+                        <h3>Section Text</h3>
+                        <p>Random text</p>
+                        </div>
+                    </div>
+                    </section>
+                </div>
+                
+                <div class="inner-content">
+                <footer id="footer" >
+                    <div class="textef1">
+                        <div id="col1" class="textes1">
+                            <h1>Vos questions</h1>
+                            <ul>
+                                <li>Le forum</></li>
+                                <li>La Q&A</></li>
+                            
+                            </ul>
+
+                        </div>
+                        <div id="col2" class="textes1">
+                            <h1 class="list1" >Notre adresse</h1>
+                            <ul>
+                            <img src="./static/image/téléchargement.png"  width="30px"><p>10 Rue de Vanves
+                                <br> Issy-les-Moulineaux <br> 
+                                </p>
+                                
+                            </ul>
+
+                        </div>
+                        <div id="col3" class="textes1">
+                        <h1>Nos réseaux sociaux</h1>
+                        <ul class="list">
+                            <li>q1</></li>
+                            <li>q1</></li>
+                            <li>q1</></li>
+                            <li>q1</></li>
+                        </ul>
+                        </div>
+                    </div>
+                        <div  class="textef1">
+                            <p  ><i>Cookies</i></p>
+                            <p  ><i>Termes d'utilisation</i></p>
+                            <p  ><i>@2022 Site officiel</i></p>
+                        </div>
+                </footer>                                                     
+                </div>
+               
+            </div>
+          
+        </div>
+
+    </div>
+    
+    <script src="./static/script/entree.js"></script>
+   
+</body>
+</html>
